597842c262c9a4f3ccfe6c5cd5a6f223d96bbe16
jaw65 <jaw65@email.uscb.edu>
2026-03-31 21:08:49 -0400

    Updated Site to support navbar and Added Navbar to Project Updated Packages like package json to get nvabar to work. Navbar is currently placeholder version

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <div className="container-fluid">
+            <Link className="navbar-brand" to="/">My App</Link>
+            </div>
+        </nav>
+    );
+
+}
