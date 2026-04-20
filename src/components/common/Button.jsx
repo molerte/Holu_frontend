a292335b63901f575e8c76f9dd7fd497137311a4
Zuri Fleurinord <ZURIF@email.sc.edu>
2026-04-20 14:53:59 -0400

    All common files are added to maintain consistency and functionality

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,13 @@
+export const Button = ({
+    children, onClick, variant = 'primary',
+    size = 'md', disabled = false, type = 'button', fullWidth = false,
+}) => (
+    <button 
+        type={type}
+        className={`btn btn--${variant} btn--${size} ${fullWidth ? 'btn-full-width' : ''}`}
+        onClick={onClick}
+        disabled={disabled}
+    >
+        {children}
+    </button>
+);
